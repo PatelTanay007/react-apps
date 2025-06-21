04776ac29da8aed533c05b115bf765e133822d4e
PatelTanay007 <115991365+PatelTanay007@users.noreply.github.com>
2025-06-22 00:18:56 +0530

    Update app files and add Pizza.js and data.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import Pizza from './Pizza';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <div className="App">
+      <h2>Pizza Menu</h2>
+      <Header />
+      <Menu />
+      <Footer />
+     </div>
+  );
+    
+}
+
+const Header =() => {
+  return (
+    <header className="App-header">
+      <h1>pizza_company_name</h1>
+    </header>
+  );
+}
+
+const Menu = () => {
+  return (
+    <>
+    <h2>our Menu</h2>
+    <Pizza />
+    </>
+  );
+}
+
+const Footer = () => {
+  return (
+    <footer className="App-footer">
+      <p>© 2025 pizza_company_name</p>
+    </footer>
   );
 }
 
---
+++ src/Pizza.js
@@ -0,0 +1,16 @@
+function Pizza() {
+  return (
+    <div className="pizza">
+      <h2>Pizza Menu</h2>
+      <ul>
+        <li>Margherita</li>
+        <li>Pepperoni</li>
+        <li>Vegetarian</li>
+        <li>BBQ Chicken</li>
+        <li>Hawaiian</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Pizza;
